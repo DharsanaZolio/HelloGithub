d421fa343e8c109c517675d5fdba67e1c584811d
SreekumarAN <zoliotech@zoliotechs-mac-mini.local>
2016-07-19 16:02:43 +0530

    Initial Commit

---
+++ HelloGitHub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloGitHub
+//
+//  Created by Zoliotech on 19/7/16.
+//  Copyright © 2016 Zoliotech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HelloGitHub/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HelloGitHub
+//
+//  Created by Zoliotech on 19/7/16.
+//  Copyright © 2016 Zoliotech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
